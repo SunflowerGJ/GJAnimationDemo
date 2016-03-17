5ae6fcdd951e7010ee3756a02ae7daf916cb90d2
GJ <gj@xdforpdeMacBook-Pro.local>
2016-03-17 15:28:41 +0800

    add CATransition & CAAnimation demoVC

---
+++ GJAnimationDemo/GJAnimationDemo/CAAnimationViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAAnimationViewController.h
+//  GJAnimationDemo
+//
+//  Created by GJ on 16/3/17.
+//  Copyright © 2016年 GJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAAnimationViewController : UIViewController
+
+@end
---
+++ GJAnimationDemo/GJAnimationDemo/CATransitionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CATransitionViewController.h
+//  GJAnimationDemo
+//
+//  Created by GJ on 16/3/17.
+//  Copyright © 2016年 GJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CATransitionViewController : UIViewController
+
+@end
